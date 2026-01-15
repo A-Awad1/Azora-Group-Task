9f7ed1f090f02fbaa67dd10166a8b466f8839b09
A-Awad1 <eng.a.awad1@gmail.com>
2026-01-15 10:35:35 +0200

    add path alias && add header icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,11 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     sourcemap: true,
   },
